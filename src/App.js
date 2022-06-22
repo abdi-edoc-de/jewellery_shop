cae71bba37735122c2fffcdbb051f85684c7659e
Abdi <se.abdulfeta.dedgba@gmail.com>
2022-06-22 22:29:53 +0300

    bug fixed in app.js'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import Card from "./Component/card/card";
-// import Nav from "./Component/nav";
-import Login from "./Component/login/login";
-import SearchBar from "./Component/searchBar/searchBar";
-// import AddProduct from "./Component/AddProduct/addProduct";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Drawer from './pages/Drawer'
 import './App.css'
 import LogInu from "./Component/login/loginU"
